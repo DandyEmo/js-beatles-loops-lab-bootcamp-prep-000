147ab670094b71772661b1660ff55d6fd81e5d51
kevin Abreu <kabreu11385@gmail.com>
2019-09-10 21:52:49 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // add solution here
+function theBeatlePlay(musicians, instruments) {
+  var emptArr = []
+  for (i = 0; i < musicians.length; i++) {
+   emptArr.push(musicians[i] + " plays " + instruments[i])
+   
+
+ } 
+ return emptArr + " plays" + instruments;
+}
+
+
+
+
